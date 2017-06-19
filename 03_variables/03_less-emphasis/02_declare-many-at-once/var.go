721b5f6da0baba1ead4491c4d68ff7ffd350bf21
eduar <eduartua@gmail.com>
2017-06-19 18:28:27 -0500

    added until scope unit

---
+++ 03_variables/03_less-emphasis/02_declare-many-at-once/var.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message string
+	var a, b, c, d int
+	a = 1
+
+	message = "Hello World!"
+
+	fmt.Printf("d variable %T \n", d)
+	fmt.Println(message, a, b, c, d)
+}
